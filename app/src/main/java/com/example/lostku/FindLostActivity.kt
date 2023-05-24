c2db62374aff6a8e523c456a1aee67a65c42fae1
ShinHB417 <heebum417@naver.com>
2023-05-24 14:41:09 +0900

    [ADD]: 액티비티 생성

---
+++ app/src/main/java/com/example/lostku/FindLostActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lostku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindLostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_lost)
+    }
+}
---
+++ app/src/main/java/com/example/lostku/LostListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lostku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LostListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lost_list)
+    }
+}
