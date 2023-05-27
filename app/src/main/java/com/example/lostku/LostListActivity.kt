a8233c2c9315a721e80c5c4edc1d98aca1689a86
ShinHB417 <heebum417@naver.com>
2023-05-27 22:33:00 +0900

    [ADD]: 분실물리스트 레이아웃 기본세팅

--- app/src/main/java/com/example/lostku/LostListActivity.kt
+++ app/src/main/java/com/example/lostku/LostListActivity.kt
@@ -2,10 +2,25 @@ package com.example.lostku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.lostku.databinding.ActivityLostListBinding
 
 class LostListActivity : AppCompatActivity() {
+    lateinit var binding : ActivityLostListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lost_list)
+        binding = ActivityLostListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initLayout()
     }
+
+    private fun initLayout() {
+        binding.apply {
+            searchBtn.setOnClickListener{
+
+            }
+        }
+    }
+
+
 }
