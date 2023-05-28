aca6c1c896743c3a30c73ac03da1c44137bdea37
Research-Mini <jm98675@konkuk.ac.kr>
2023-05-29 03:22:00 +0900

    [ADD] MainActivity binding, ClickListener 추가

--- app/src/main/java/com/example/lostku/MainActivity.kt
+++ app/src/main/java/com/example/lostku/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.lostku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.lostku.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding :ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initLayout()
+    }
+
+    private fun initLayout() {
+        binding.apply {
+            ListBtn.setOnClickListener {
+                Toast.makeText(this@MainActivity, "ListBtn!!!", Toast.LENGTH_SHORT).show()
+
+            }
+
+            FindBtn.setOnClickListener {
+                Toast.makeText(this@MainActivity, "FindBtn!!!", Toast.LENGTH_SHORT).show()
+            }
+
+            UploadBtn.setOnClickListener {
+                Toast.makeText(this@MainActivity, "UploadBtn!!!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
