985565ebafac7c676db484a6406b604750dd401c
Research-Mini <jm98675@konkuk.ac.kr>
2023-05-29 07:13:44 +0900

    [ADD] FindLostActivity.kt 프로토타입 완성(list.txt, tablelayout) 추가

--- app/src/main/java/com/example/lostku/FindLostActivity.kt
+++ app/src/main/java/com/example/lostku/FindLostActivity.kt
@@ -2,6 +2,7 @@ package com.example.lostku
 
 
 import android.app.ProgressDialog.show
+import android.graphics.Color
 import android.location.Geocoder
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
@@ -9,15 +10,68 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import com.example.lostku.databinding.ActivityFindLostBinding
+import java.util.*
 
 class FindLostActivity : AppCompatActivity() {
     lateinit var binding: ActivityFindLostBinding
+
+    var LData : ArrayList<SaveLocation> = ArrayList()
+    var location=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFindLostBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initlists()
         initLayout()
     }
+
+    private fun makeTable() {
+
+        LData.sortBy { it.location }
+        val tableLayout = binding.tableLayout
+        for (address in LData) {
+            val tableRow = TableRow(this)
+            val textView1 = TextView(this)
+            textView1.setTextSize(30F)
+            textView1.text = address.name
+            textView1.setBackgroundResource(R.drawable.tableline)
+            val textView2 = TextView(this)
+            textView2.text = address.location.toString()+"M"
+            textView2.setTextSize(33F)
+            textView2.setBackgroundResource(R.drawable.tableline)
+            tableRow.addView(textView1)
+            tableRow.addView(textView2)
+
+            tableLayout.addView(tableRow)
+        }
+    }
+    private fun ClearTable() {
+
+
+        val tableLayout = binding.tableLayout
+        //tableLayout.removeAllViews()
+        tableLayout.removeViews(1,tableLayout.childCount-1)
+
+    }
+
+    private fun initlists() {
+        ClearTable()
+        LData.removeAll(LData)
+        val scan = Scanner(resources.openRawResource(R.raw.lists))
+
+        while (scan.hasNextLine())
+        {
+            val name = scan.nextLine()
+            val locations = scan.nextLine().toInt() - location
+            LData.add(SaveLocation(name, locations))
+
+        }
+
+        makeTable()
+
+    }
+
 
     private fun initLayout() {
         var sData = resources.getStringArray(R.array.building)
@@ -25,6 +79,9 @@ class FindLostActivity : AppCompatActivity() {
             ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, sData)
         binding.buildings.adapter = sadapter
         binding.buildings.setOnItemSelectedListener(CustomOnItemSelectedListener())
+
+
+
 
 // LocationManager 객체를 가져옵니다.
 //        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
@@ -37,21 +94,11 @@ class FindLostActivity : AppCompatActivity() {
 //        val addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1)
 //
 //// 검색된 주소를 표에 표시합니다.
-//        val tableLayout = findViewById<TableLayout>(R.id.tableLayout)
-//        for (address in addresses) {
-//            val tableRow = TableRow(this)
-//            val textView1 = TextView(this)
-//            textView1.text = address.featureName
-//            val textView2 = TextView(this)
-//            textView2.text = address.getAddressLine(0)
-//            tableRow.addView(textView1)
-//            tableRow.addView(textView2)
-//            tableLayout.addView(tableRow)
-//        }
-//    }
 
 
-    }
+//
+//
+       }
 
     inner class CustomOnItemSelectedListener: AdapterView.OnItemSelectedListener{
         override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
@@ -59,11 +106,23 @@ class FindLostActivity : AppCompatActivity() {
             Toast.LENGTH_SHORT).show()
             binding.recyclerView.adapter = MyCafeteriaAdapter(CData)*/
             when (position) {
-                0 -> Toast.makeText(this@FindLostActivity,"0!!!",Toast.LENGTH_SHORT).show()
-                    //binding.recyclerView.adapter = adapter1
-                1 -> Toast.makeText(this@FindLostActivity,"1!!!",Toast.LENGTH_SHORT).show()
+                0 -> {Toast.makeText(this@FindLostActivity,"0!!!",Toast.LENGTH_SHORT).show()
+                        location = 5
+                        initlists()
 
-                2 -> Toast.makeText(this@FindLostActivity,"2!!!",Toast.LENGTH_SHORT).show()
+                        }
+                    //binding.recyclerView.adapter = adapter1
+                1 -> {
+                    Toast.makeText(this@FindLostActivity, "1!!!", Toast.LENGTH_SHORT).show()
+                    location = 7
+                        initlists()
+                    }
+
+                2 -> {
+                    Toast.makeText(this@FindLostActivity, "2!!!", Toast.LENGTH_SHORT).show()
+                    location = 9
+                        initlists()
+                    }
             }
         }
 
