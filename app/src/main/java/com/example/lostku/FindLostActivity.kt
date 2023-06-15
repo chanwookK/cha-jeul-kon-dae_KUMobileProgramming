b18a6958a844f0d2e5b324c09c85b6f45e3a82ec
Research-Mini <jm98675@konkuk.ac.kr>
2023-06-15 17:51:15 +0900

    [ADD] GPS 현재 위치 받아오기 기능 추가

--- app/src/main/java/com/example/lostku/FindLostActivity.kt
+++ app/src/main/java/com/example/lostku/FindLostActivity.kt
@@ -1,7 +1,10 @@
 package com.example.lostku
 
 
+import android.Manifest
 import android.app.ProgressDialog.show
+import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.location.Geocoder
 import android.location.LocationManager
@@ -9,14 +12,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.lostku.databinding.ActivityFindLostBinding
 import java.util.*
 
 class FindLostActivity : AppCompatActivity() {
-    lateinit var binding: ActivityFindLostBinding
 
+    lateinit var binding: ActivityFindLostBinding
     var LData : ArrayList<SaveLocation> = ArrayList()
     var location=0
+    var cur_lat: Double? = null
+    var cur_lon: Double? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFindLostBinding.inflate(layoutInflater)
@@ -25,6 +33,8 @@ class FindLostActivity : AppCompatActivity() {
         initlists()
         initLayout()
     }
+
+
 
     private fun makeTable() {
 
@@ -73,7 +83,19 @@ class FindLostActivity : AppCompatActivity() {
     }
 
 
+
     private fun initLayout() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
+        } else {
+//            val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER) ?: locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+            val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val loc_Current = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)?: locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+            cur_lat = loc_Current?.latitude
+            cur_lon = loc_Current?.longitude
+            Toast.makeText(this, "Latitude: $cur_lat, Longitude: $cur_lon", Toast.LENGTH_LONG).show()
+        }
         var sData = resources.getStringArray(R.array.building)
         var sadapter =
             ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, sData)
@@ -83,12 +105,14 @@ class FindLostActivity : AppCompatActivity() {
 
 
 
+
 // LocationManager 객체를 가져옵니다.
 //        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
 //
 //// 사용자의 현재 위치를 가져옵니다.
-//        val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+//        locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
 //
+
 //// 가까운 장소를 검색합니다.
 //        val geocoder = Geocoder(this)
 //        val addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1)
@@ -96,8 +120,6 @@ class FindLostActivity : AppCompatActivity() {
 //// 검색된 주소를 표에 표시합니다.
 
 
-//
-//
        }
 
     inner class CustomOnItemSelectedListener: AdapterView.OnItemSelectedListener{
